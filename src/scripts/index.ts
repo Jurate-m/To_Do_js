fee3e9aa18aaf54f5d93595be6d29f2860d9f495
Jurate-M <dev.jurate@gmail.com>
2025-12-29 23:26:43 +0200

    Render tasks based on filter

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,27 +1,42 @@
 import * as createTask from "./actions/createTask.ts";
 import * as tasksMutation from "./mutations/tasks.ts";
 import * as tasksTemplate from "./actions/tasksTemplate.ts";
+import {AVAILABLE_PATHS} from "./config.js";
+
+const handleFiltering = function () {
+  const path = window.location.hash.slice(1).trim();
+  if (!path) tasksTemplate.renderTasks(tasksMutation.tasks);
+  if (!AVAILABLE_PATHS.includes(path)) return;
+  // throw error
+  const filteredTasks = tasksMutation.filteredTasks(path);
+  tasksTemplate.renderTasks(filteredTasks);
+};
 
 const handleTaskSumbit = function () {
   const checkInputValidity = createTask.checkIfValidInput();
   if (!checkInputValidity) return;
   const task = createTask.getTaskAttr();
   tasksMutation.addTask(task);
-  tasksTemplate.renderTasks(tasksMutation.tasks);
+  handleFiltering();
 };
 
 const handleTaskRemove = function (id: number) {
   tasksMutation.removeTask(id);
-  tasksTemplate.renderTasks(tasksMutation.tasks);
+  handleFiltering();
 };
 
 const handleTaskComplete = function (id: number) {
   tasksMutation.toggleCompleteTask(id);
-  tasksTemplate.renderTasks(tasksMutation.tasks);
+  handleFiltering();
+};
+
+const hashChangeListener = function () {
+  window.addEventListener("hashchange", handleFiltering);
 };
 
 const init = function () {
-  tasksTemplate.renderTasks(tasksMutation.tasks);
+  handleFiltering();
+  hashChangeListener();
   createTask.handleSubmit(handleTaskSumbit);
   tasksTemplate.removeTask(handleTaskRemove);
   tasksTemplate.markCompleteTask(handleTaskComplete);
